68dad857de51c047d6b681bf35f87deced0ee34d
nakitaki <nadya.mishevaa@gmail.com>
2023-05-08 17:12:45 +0300

    queue

---
+++ java-university-exercise/src/main/java/e08_Collection/ArrayDeque/Queue/HotPotato.java
@@ -0,0 +1,31 @@
+package e08_Collection.ArrayDeque.Queue;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class HotPotato {
+    //hot potato -> rmeove the person on the last turn(numberOfTurns)
+    //Maria Ana John with number of turns 2; first will remove ana, after that Maria and John is the wiinner
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] children = scanner.nextLine().split(" ");
+        int numberOfTurns = Integer.parseInt(scanner.nextLine());
+        ArrayDeque<String> childrenQueue = new ArrayDeque<>();
+
+        for (String child : children) {
+            childrenQueue.offer(child);
+        }
+
+        while (childrenQueue.size() != 1) {
+            for (int i = 1; i < numberOfTurns; i++) {
+                String currentChild = childrenQueue.poll();
+                childrenQueue.offer(currentChild);
+            }
+            String childRemove = childrenQueue.poll();
+            System.out.println("Removed " + childRemove);
+        }
+        String lastChild = childrenQueue.peek();
+        System.out.println("Last is " + lastChild);
+
+    }
+}
---
+++ java-university-exercise/src/main/java/e08_Collection/ArrayDeque/Queue/PrinterQueue.java
@@ -0,0 +1,30 @@
+package e08_Collection.ArrayDeque.Queue;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class PrinterQueue {
+    //enter files to print -> cancel command == cancel the first file you entered; print command == print all files left
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        ArrayDeque<String> files = new ArrayDeque<>();
+
+        while(!input.equals("print")){
+            if(input.equals("cancel")){
+                if(files.isEmpty()){
+                    System.out.println("Printer is on standby");
+                } else {
+                    String canceledFile = files.poll();
+                    System.out.println("Canceled " + canceledFile);
+                }
+            } else{
+                files.offer(input);
+            }
+            input = scanner.nextLine();
+        }
+        for (String file : files) {
+            System.out.println(file);
+        }
+    }
+}
